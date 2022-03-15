fef8f7daa8946e421a3fa04b28296d86e0581f37
Luciano Cheng <luciano.cheng@gmail.com>
2022-03-14 19:45:31 -0700

    Convert Scale Gestur Detector logic

--- photoeditor/src/main/java/ja/burhanrashid52/photoeditor/OnPhotoEditorListener.kt
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/OnPhotoEditorListener.kt
@@ -1,95 +1,94 @@
-package ja.burhanrashid52.photoeditor;
+package ja.burhanrashid52.photoeditor
 
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.accessibility.AccessibilityManager;
+import android.view.MotionEvent
+import android.view.View
 
 /**
- * @author <a href="https://github.com/burhanrashid52">Burhanuddin Rashid</a>
+ * @author [Burhanuddin Rashid](https://github.com/burhanrashid52)
  * @version 0.1.1
  * @since 18/01/2017
- * <p>
+ *
+ *
  * This are the callbacks when any changes happens while editing the photo to make and custimization
  * on client side
- * </p>
+ *
  */
-public interface OnPhotoEditorListener {
-
+interface OnPhotoEditorListener {
     /**
      * When user long press the existing text this event will trigger implying that user want to
-     * edit the current {@link android.widget.TextView}
+     * edit the current [android.widget.TextView]
      *
      * @param rootView  view on which the long press occurs
      * @param text      current text set on the view
      * @param colorCode current color value set on view
      */
-    void onEditTextChangeListener(View rootView, String text, int colorCode);
+    fun onEditTextChangeListener(rootView: View?, text: String?, colorCode: Int)
 
     /**
-     * This is a callback when user adds any view on the {@link PhotoEditorView} it can be
+     * This is a callback when user adds any view on the [PhotoEditorView] it can be
      * brush,text or sticker i.e bitmap on parent view
      *
      * @param viewType           enum which define type of view is added
      * @param numberOfAddedViews number of views currently added
      * @see ViewType
      */
-    void onAddViewListener(ViewType viewType, int numberOfAddedViews);
+    fun onAddViewListener(viewType: ViewType?, numberOfAddedViews: Int)
 
     /**
-     * This is a callback when user remove any view on the {@link PhotoEditorView} it happens when usually
+     * This is a callback when user remove any view on the [PhotoEditorView] it happens when usually
      * undo and redo happens or text is removed
      *
      * @param viewType           enum which define type of view is added
      * @param numberOfAddedViews number of views currently added
      */
-    void onRemoveViewListener(ViewType viewType, int numberOfAddedViews);
+    fun onRemoveViewListener(viewType: ViewType?, numberOfAddedViews: Int)
 
     /**
      * A callback when user start dragging a view which can be
-     * any of {@link ViewType}
+     * any of [ViewType]
      *
      * @param viewType enum which define type of view is added
      */
-    void onStartViewChangeListener(ViewType viewType);
+    fun onStartViewChangeListener(viewType: ViewType?)
 
     /**
      * A callback when a user moves *or* resizes a view which can be
-     * any of {@link ViewType}
+     * any of [ViewType]
      *
      * @param viewType
      */
-    void onMoveViewChangeListener(ViewType viewType);
+    fun onMoveViewChangeListener(viewType: ViewType?)
 
     /**
      * A callback when user stop/up touching a view which can be
-     * any of {@link ViewType}
+     * any of [ViewType]
      *
      * @param viewType enum which define type of view is added
      */
-    void onStopViewChangeListener(ViewType viewType);
+    fun onStopViewChangeListener(viewType: ViewType?)
 
     /**
      * A callback when the user touches the screen.
      *
      * @param event the MotionEvent associated to the touch.
      */
-    void onTouchSourceImage(MotionEvent event);
+    fun onTouchSourceImage(event: MotionEvent?)
 
     /**
-     * This is a callback when user rotates all views on the {@link PhotoEditorView}
+     * This is a callback when user rotates all views on the [PhotoEditorView]
      *
      */
-    void onRotateViewListener();
+    fun onRotateViewListener()
 
     /**
-     * This is a callback when user mirrors any view on the {@link PhotoEditorView}
+     * This is a callback when user mirrors any view on the [PhotoEditorView]
      */
-    void onMirrorViewListener();
+    fun onMirrorViewListener()
 
     /**
      * A callback when user focusing on a sticker
      *
      * @param view view that is in focus.
      */
-    void onInFocusViewChangeListener(View view);
+    fun onInFocusViewChangeListener(view: View?)
 }
