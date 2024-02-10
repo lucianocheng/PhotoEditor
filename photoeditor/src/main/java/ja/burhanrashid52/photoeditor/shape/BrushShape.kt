e364ca0c992fa015dadedc01f1de426bb2de477a
Evgeny Kapralov <fleissig@users.noreply.github.com>
2024-02-10 08:43:46 +0600

    BrushShape: Add ability to adjust canvas to support Crop (#36)

--- photoeditor/src/main/java/ja/burhanrashid52/photoeditor/shape/BrushShape.kt
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/shape/BrushShape.kt
@@ -6,9 +6,30 @@ import android.util.Log
 import kotlin.math.abs
 
 class BrushShape : AbstractShape("BrushShape") {
+    var offsetX = 0f
+        private set
+    var offsetY = 0f
+        private set
+    private var scaleX = 1f
+    private var scaleY = 1f
+
+    fun adjustCanvas(newOffsetX: Float, newOffsetY: Float, newScaleX: Float, newScaleY: Float) {
+        scaleX *= newScaleX
+        scaleY *= newScaleY
+
+        offsetX = newOffsetX
+        offsetY = newOffsetY
+    }
 
     override fun draw(canvas: Canvas, paint: Paint) {
+        canvas.save()
+
+        canvas.translate(offsetX, offsetY)
+        canvas.scale(scaleX, scaleY)
+
         canvas.drawPath(path, paint)
+
+        canvas.restore()
     }
 
     override fun startShape(x: Float, y: Float) {
